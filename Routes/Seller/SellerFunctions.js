5c603bf474650a11d65147ea53af4861a39bf023
mohit <mohitratan2003@gmail.com>
2024-04-23 23:58:41 +0530

    add to inventory need to updated

--- Routes/Seller/SellerFunctions.js
+++ Routes/Seller/SellerFunctions.js
@@ -2,95 +2,17 @@ const express = require('express');
 const router = express.Router();
 const Auth = require('../../Middlewares/Auth');
 const Products = require('../../DataModels/Products');
+const Seller = require('../../DataModels/Seller');
+//Add Item to inventroy 
 
-//Add Item
-// router.post('/additem',Auth,async(req,res)=>{
-//     const {name,product_description,price,img} = req.body;
-//     const token = req.user;
-//     const tokenid = token.id;
-//     // console.log(token.id);
-//     try {
-//         const checkproduct = await Products.findOne({name,product_description,price,img});
-//         console.log(checkproduct)
-//         const status = false;
-//         if(checkproduct){
-//             const pid = checkproduct._id;
-//             return res.status(401).json({"message":"Item exists",status});
-//         }
-//         else{
-//             const product = await Products.create({
-//                 name,
-//                 product_description,
-//                 inventory,
-//                 price,
-//                 img,
-//                 userId:token.id,
-//             })
-//             // console.log(product);
-//             return res.status(200).json({"message":"Added successfully"});
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// });
-
-// //reduce the inventory
-// router.post('/decrement/:id',Auth,async(req,res)=>{
-//     const itemid = req.params.id;
-//     const item = await Products.findOne({_id:itemid});
-//     if(!item){
-//         return res.status(400).json({"message":"item not found"});
-//     }
-//     else if(req.user.id != item.userId){
-//         console.log(req.user.id);
-//         console.log(item.userId);
-//         return res.status(400).json({"message":"Permission denied"});
-//     }
-//     else{
-//         const inventory = item.inventory - 1;
-//         console.log(inventory);
-//         await Products.findOneAndUpdate({_id:itemid},{inventory:inventory});
-//         return res.status(201).json({"message":"Decrement done"}); 
-//     }
-// })
-
-// //increase the inventory
-// router.post('/increment/:id',Auth,async(req,res)=>{
-//     const itemid = req.params.id;
-//     const item = await Products.findOne({_id:itemid});
-//     if(!item){
-//         return res.status(400).json({"message":"item not found"});
-//     }
-//     else if(req.user.id != item.userId){
-//         console.log(req.user.id);
-//         console.log(item.userId);
-//         return res.status(400).json({"message":"Permission denied"});
-//     }
-//     else{
-//         const inventory = item.inventory + 1;
-//         console.log(inventory);
-//         await Products.findOneAndUpdate({_id:itemid},{inventory:inventory});
-//         return res.status(201).json({"message":"Increment done"}); 
-//     }
-// })
-
-// //delete the item
-// router.post('/delete/:id',Auth,async(req,res)=>{
-//     const itemid = req.params.id;
-//     const item = await Products.findOne({_id:itemid});
-//     if(!item){
-//         return res.status(400).json({"message":"item not found"});
-//     }
-//     else if(req.user.id != item.userId){
-//         console.log(req.user.id);
-//         console.log(item.userId);
-//         return res.status(400).json({"message":"Permission denied"});
-//     }
-//     else{
-//         await Products.findByIdAndDelete(req.params.id);
-//         return res.status(201).json({"message":"delete done"}); 
-//     }
-// })
+router.post('/inventory/:id',async(req,res)=>{
+    console.log(req.params.id);
+    try {
+        
+    } catch (error) {
+        
+    }
+})
 
 // Products ADD bt admin
 
@@ -102,7 +24,7 @@ router.post('/additems',async(req,res)=>{
             product_description,
             price,
             img,
-            approved:true,
+            approved:false,
         })
         return res.status(201).json({"message":"ADDED"});
     } catch (error) {
