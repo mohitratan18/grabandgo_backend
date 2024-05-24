e56b59b562cce1d59b7c8469f17b2284d1993da7
mohit <mohitratan2003@gmail.com>
2024-05-25 04:35:48 +0800

    added mongodb server

--- Database.js
+++ Database.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    mongoose.connect("mongodb://localhost:27017/grabandgo", {
-      useNewUrlParser: true,
-    });
+    mongoose.connect(
+      "mongodb+srv://mohitratan2003:mohitratan6317@cluster0.jzlaenv.mongodb.net/GG?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+      }
+    );
     console.log("Database connected");
   } catch (error) {
     console.log(error);
